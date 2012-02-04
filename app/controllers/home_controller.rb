d7ae6f5c2315735cdfcd2ca58189475f534b97eb
Akash Patel <thekash@gmail.com>
2012-02-04 12:36:54 -0800

    beta email signup added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
   
   def beta_signup
-    debugger
-    params
-    
+    email = params['beta_signup_input']
+    bs = BetaSignup.new(:email => email)
+    if (bs.save)
+      render :nothing => true
+    else
+      render :json => 'Email Signup Failed.', :status => 500
+    end
   end
   
 end
