8ce0b594848e3b78e870e3298b19878caa2b417c
Akash Patel <thekash@gmail.com>
2012-04-10 02:39:28 -0700

    added json masonry to user profile page

--- app/models/user_asset.rb
+++ app/models/user_asset.rb
@@ -3,8 +3,10 @@ class UserAsset < ActiveRecord::Base
   belongs_to  :user_profile
   
   has_attached_file :image,
-                    :styles => {
-                        :thumb     => "100x100##",
-                        :pinterest => "260>"
-                    }
+      :styles => {
+          :thumb     => "100x100##",
+          :pinterest => "300x300>",
+      }
+  
+  validates_attachment :image, :presence => true,                  
 end
