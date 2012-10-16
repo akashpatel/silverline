1301a0b74e18ed0e6f516315124358b9c1f057d9
Akash Patel <thekash@gmail.com>
2012-10-16 14:04:55 -0700

    added author dropdown when creating a new quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,8 @@
 class Quote < ActiveRecord::Base
   belongs_to :author
 
+  validates :description, :author_id, :tag_list, :presence => true
+
   acts_as_taggable
 
   scope :previous, lambda { |p| {:conditions => ["id < ?", p.id], :limit => 1, :order => "id DESC"} }
