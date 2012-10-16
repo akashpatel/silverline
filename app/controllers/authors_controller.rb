845ed9f298a8a5f0c9906485848bc0986a42e973
Akash Patel <thekash@gmail.com>
2012-10-16 03:00:22 -0700

    be able to edit / update an author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,4 +7,14 @@ class AuthorsController < ApplicationController
     redirect_to new_author_path
   end
 
+  def update
+    author = Author.find(params[:id])
+    author.update_attributes(params[:author])
+    redirect_to edit_author_path(author)
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
 end
